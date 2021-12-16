c1440e54338d94f549d40fa26db1a489d3b388d8
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-16 14:23:57 -0700

    commit tree a79f61eac25982751e7755312312a864a32fbea6

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+Class Person 
+    attr_reader :person
+
+    def initialize (name)
+        @name = name
+    end
+
+# initialize accepts an argument of name, stored inside a name instance variable.
+
+end
